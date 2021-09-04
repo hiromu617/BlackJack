b5034c5193d5adbba9be4f0a6c2128e0866df588
hiro <hiromhiro617@gmail.com>
2021-09-04 11:40:56 +0900

    impl Card.ts

---
+++ src/model/Card.ts
@@ -0,0 +1,16 @@
+type Suit = 'H' | 'D' | 'C' | 'S';
+type Rank = 'A' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 'J' | 'Q' | 'K';
+
+class Card {
+  constructor(private readonly suit: Suit, private readonly rank: Rank) {}
+
+  // カードのランクを数値で返す
+  getRankNumber(): number {
+    // Aはとりあえず11を返す
+    if (this.rank === 'A') return 11;
+    else if (this.rank === 'J' || this.rank === 'Q' || this.rank === 'K') {
+      return 10;
+    }
+    return this.rank;
+  }
+}
